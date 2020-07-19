d285b7e74122817763541ffec6b652c8e5f8f2ab
Jesus Gutierrez <jesus.gutierrez.b@outlook.com>
2020-07-19 11:31:52 -0700

    Finished script

--- sketch.js
+++ sketch.js
@@ -1,11 +1,30 @@
 // access the 'main' element
 const grid = document.querySelector("main");
-//const cells = document.querySelectorAll("div");
-
-createGrid(16);
+// access the clear button
+const clear = document.querySelector("#clear");
+// default grid created when the webpage launches
+createGrid();
+// What happens when the clear button is clicked
+clear.addEventListener("click", () => {
+    let number;
+    // delete the current grid
+    while(grid.hasChildNodes()){
+        let child = grid.lastChild;
+        grid.removeChild(child);
+    }    
+    // ask the user for a new number to build a new grid
+    number = prompt(`Please enter a number to create a 'number x number' grid.
+(up to 50 x50)`, "1");
+    if(number >= 0 && number <= 50){
+        createGrid(number);
+    // in case the user enters an invalid value
+    } else{
+        createGrid();
+    }
+});
 // function to create the grid inside the 'main' element
 // with a default size of 0 squares
-function createGrid(dimensions = 0){
+function createGrid(dimensions = 2){
     // Variable to handle each individual cell of the grid
     let square;
     // total number of squares in the grid
@@ -19,14 +38,11 @@ function createGrid(dimensions = 0){
         square = document.createElement("div");
         // cell's style
         square.style.cssText = "border: 0.5px solid black";
+        // create a hover event to change the background color
+        square.addEventListener("mouseover", (e) => {
+            e.target.style.background= "gold";
+        });
         // append cell to the grid
         grid.appendChild(square);
     } // end for
 } // end createGrid
-/*
-cells.forEach((div) => {
-    div.addEventListener("mouseover", () =>{
-        div.style.cssText = "background-color: gold";
-    });
-});
-*/
